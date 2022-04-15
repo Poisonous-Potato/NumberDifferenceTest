be3849124a0cf439af108c00f21e133599fa550a
reyrizki <reynaldirizki97.rr@gmail.com>
2022-04-15 13:09:31 +0700

    :recycle: Refactor NumberDifferenceTest

---
+++ test/program/NumberDifference/GroupTest.java
@@ -0,0 +1,86 @@
+package program.NumberDifference;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GroupTest extends NumberDifferenceTest {
+    private String messageBuilder(int diff) {
+        String message;
+
+        message = "Difference: " + diff;
+
+        if (diff == 0) {
+            message = message + "\nNon group, There's no difference";
+        } else if ((diff >= 1) && (diff <= 10)) {
+            message = message + "\nGroup 1, Small Difference";
+        } else if ((diff >= 11) && (diff <= 50)) {
+            message = message + "\nGroup 2, Medium Difference";
+        } else if (diff >= 51) {
+            message = message + "\nGroup 3, Large Difference";
+        } 
+
+        return message;
+    }
+    
+	@Test
+	public void nonGroup_01() { // TC01
+		// (1) setup (arrange, build)
+		int diff;
+		String actual, expected;
+		diff = 0;
+        expected = messageBuilder(diff);
+
+		// (2) exercise (act, operate)
+		actual = sut.groupingDifference(diff);
+
+		// (3) verify (assert, check)
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void group1_01() { // TC10
+		// (1) setup (arrange, build)
+		int diff;
+		String actual, expected;
+		diff = 5;
+        expected = messageBuilder(diff);
+
+		// (2) exercise (act, operate)
+		actual = sut.groupingDifference(diff);
+
+		// (3) verify (assert, check)
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void group2_01() { // TC07
+		// (1) setup (arrange, build)
+		int diff;
+		String actual, expected;
+		diff = 25;
+        expected = messageBuilder(diff);
+
+		// (2) exercise (act, operate)
+		actual = sut.groupingDifference(diff);
+
+		// (3) verify (assert, check)
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void group3_01() { // TC03
+		// (1) setup (arrange, build)
+		int diff;
+		String actual, expected;
+		diff = 100;
+        expected = messageBuilder(diff);
+
+		// (2) exercise (act, operate)
+		actual = sut.groupingDifference(diff);
+
+		// (3) verify (assert, check)
+		assertEquals(expected, actual);
+	}
+
+}
--- test/program/NumberDifference/NumberDifferenceTest.java
+++ test/program/NumberDifference/NumberDifferenceTest.java
@@ -1,12 +1,9 @@
 package program.NumberDifference;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
-import org.junit.Test;
 
 public class NumberDifferenceTest {
-	private CalcNUmberDifference sut;
+	protected CalcNUmberDifference sut;
 	
 	/**
 	 * Setup = inisiasi sebelum test dijalankan, preparation object
@@ -19,119 +16,6 @@ public class NumberDifferenceTest {
 	@Before
 	public void setUp() throws Exception {
 		sut = new CalcNUmberDifference();
-	}
-
-	@Test
-	public void nOutOfRange_01() { // TC18
-		// (1) setup (arrange, build)
-		int n;
-		boolean actual, expected;
-		n = 0; // n below range
-		expected = false;
-		
-		// (2) exercise (act, operate)
-		actual = sut.validateRangeInputMaxDeret(n);
-		
-		// (3) verify (assert, check)
-		assertEquals(expected, actual);
-	}
-	
-	@Test
-	public void nOutOfRange_02() { // TC19
-		// (1) setup (arrange, build)
-		int n;
-		boolean actual, expected;
-		n = 11; // n above range
-		expected = false;
-		
-		// (2) exercise (act, operate)
-		actual = sut.validateRangeInputMaxDeret(n);
-		
-		// (3) verify (assert, check)
-		assertEquals(expected, actual);
-	}
-	
-	@Test
-	public void nInsideOfRange_01() {
-		// (1) setup (arrange, build)
-		int jumlahDeretBil;
-		boolean actual, expected;
-		jumlahDeretBil = 5; // berada pada range 1-10
-		expected = true;
-		
-		// (2) exercise (act, operate)
-		actual = sut.validateRangeInputMaxDeret(jumlahDeretBil);
-		
-		// (3) verify (assert, check)
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void nonGroup_01() { // TC01
-		// (1) setup (arrange, build)
-		int diff;
-		String actual, expected;
-		diff = 0;
-		expected = "Difference: " + diff; 
-		expected = expected + "\nNon group, ";
-		expected = expected + "There's no difference";
-
-		// (2) exercise (act, operate)
-		actual = sut.groupingDifference(diff);
-
-		// (3) verify (assert, check)
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void group1_01() { // TC10
-		// (1) setup (arrange, build)
-		int diff;
-		String actual, expected;
-		diff = 5;
-		expected = "Difference: " + diff; 
-		expected = expected + "\nGroup 1, ";
-		expected = expected + "Small Difference";
-
-		// (2) exercise (act, operate)
-		actual = sut.groupingDifference(diff);
-
-		// (3) verify (assert, check)
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void group2_01() { // TC07
-		// (1) setup (arrange, build)
-		int diff;
-		String actual, expected;
-		diff = 25;
-		expected = "Difference: " + diff; 
-		expected = expected + "\nGroup 2, ";
-		expected = expected + "Medium Difference";
-
-		// (2) exercise (act, operate)
-		actual = sut.groupingDifference(diff);
-
-		// (3) verify (assert, check)
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void group3_01() { // TC03
-		// (1) setup (arrange, build)
-		int diff;
-		String actual, expected;
-		diff = 100;
-		expected = "Difference: " + diff; 
-		expected = expected + "\nGroup 3, ";
-		expected = expected + "Large Difference";
-
-		// (2) exercise (act, operate)
-		actual = sut.groupingDifference(diff);
-
-		// (3) verify (assert, check)
-		assertEquals(expected, actual);
 	}
 
 }
